52e0c58b76b6d496ab099bf1afb69ffa6e085b9a
pan <370758158@qq.com>
2021-12-04 15:34:34 +0800

    update the pushplus

--- pushplus.sh
+++ pushplus.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-input=passwd.txt
-exec 6<&0
-exec 0<$input
 git push github master  
 while [ $? -ne 0 ]
 do
     git push github master  
-done
+done<<EOF
+370758158@qq.com\n
+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\n
+EOF
+
