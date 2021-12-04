2b2efae86fe672235ad3544deec923b87eda4694
pan <370758158@qq.com>
2021-12-04 23:16:26 +0800

    2021-12-4 23:16

--- pushplus.sh
+++ pushplus.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+exec 6<&0
+exec 0<passwd.txt
+IFS_FLAG=$IFS
+IFS=','
+input=passwd.txt
 git push github master  
 while [ $? -ne 0 ]
 do
     git push github master  
-done<<EOF
-370758158@qq.com\n
-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\n
-EOF
+done<$input
 
+IFS=$IFS_FLAG
